b11a8bb33655201c9649258986668a0f0831fa62
eocron <ad-company@mail.ru>
2021-01-16 02:16:13 +0300

    Added tree traversal algorithms.

---
+++ Algorithm/Tree/TreeTraversalExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm.Tree
+{
+    /// <summary>
+    /// https://en.wikipedia.org/wiki/Tree_traversal
+    /// </summary>
+    public static class TreeTraversalExtensions
+    {
+        /// <summary>
+        /// RNL
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <param name="childrenProvider"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TraverseReverseInOrder<T>(this T root, Func<T, IEnumerable<T>> childrenProvider)
+        {
+            var stack = new Stack<T>();
+            stack.Push(root);
+            while(stack.Count > 0)
+            {
+                var item = stack.Pop();
+                yield return item;
+                var children = childrenProvider(item);
+                if(children != null)
+                {
+                    foreach (var c in children)
+                        stack.Push(c);
+                }
+            }
+        }
+
+        /// <summary>
+        /// NLR
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <param name="childrenProvider"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TraversePreOrder<T>(this T root, Func<T, IEnumerable<T>> childrenProvider)
+        {
+            return TraverseReverseInOrder(root, (x) => childrenProvider(x)?.Reverse());
+        }
+
+        /// <summary>
+        /// LRN
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <param name="childrenProvider"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TraversePostOrder<T>(this T root, Func<T, IEnumerable<T>> childrenProvider)
+        {
+            return TraverseReverseInOrder(root, childrenProvider).Reverse();
+        }
+
+        /// <summary>
+        /// BFS
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <param name="childrenProvider"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> TraverseLevelOrder<T>(this T root, Func<T, IEnumerable<T>> childrenProvider)
+        {
+            var queue = new Queue<T>();
+            queue.Enqueue(root);
+            while(queue.Count > 0)
+            {
+                var item = queue.Dequeue();
+                    yield return item;
+                var children = childrenProvider(item);
+                if (children != null)
+                {
+                    foreach (var c in children)
+                    {
+                        queue.Enqueue(c);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NTests/TreeTraversalTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Algorithm.Tree;
+using System.Linq;
+
+namespace NTests
+{
+    [TestFixture]
+    public class TreeTraversalTests
+    {
+        //private Dictionary<char, List<char>> Tree = new Dictionary<char, List<char>> { 
+        //    { 'D', new List<char> { 'C', 'F' } },
+        //    { 'C', new List<char> { 'A', 'B' } },
+        //    { 'F', new List<char> { 'E', 'G' } }
+        //};
+
+        private Dictionary<char, List<char>> Tree = new Dictionary<char, List<char>> {
+            { 'F', new List<char> { 'B', 'G' } },
+            { 'B', new List<char> { 'A', 'D' } },
+            { 'D', new List<char> { 'C', 'E' } },
+                        { 'G', new List<char> { 'I' } },
+                                    { 'I', new List<char> { 'H' } }
+        };
+
+        private IEnumerable<char> GetChildren(char c)
+        {
+            List<char> res;
+            if (Tree.TryGetValue(c, out res))
+                return res;
+            return null;
+        }
+
+        [Test]
+        public void ReverseInOrder_RNL()
+        {
+            var root = 'F';
+            var result = new string(root.TraverseReverseInOrder(GetChildren).ToArray());
+            Assert.AreEqual("FGIHBDECA", result);
+        }
+
+        [Test]
+        public void PreOrder_NLR()
+        {
+            var root = 'F';
+            var result = new string(root.TraversePreOrder(GetChildren).ToArray());
+            Assert.AreEqual("FBADCEGIH", result);
+        }
+
+        [Test]
+        public void PostOrder_LRN()
+        {
+            var root = 'F';
+            var result = new string(root.TraversePostOrder(GetChildren).ToArray());
+            Assert.AreEqual("ACEDBHIGF", result);
+        }
+
+        [Test]
+        public void LevelOrder_BFS()
+        {
+            var root = 'F';
+            var result = new string(root.TraverseLevelOrder(GetChildren).ToArray());
+            Assert.AreEqual("FBGADICEH", result);
+        }
+    }
+}
