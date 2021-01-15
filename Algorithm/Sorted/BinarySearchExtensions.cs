1ae31f2ddf3b03de08b4e43ffe46e4b99a5f1d1b
eocron <ad-company@mail.ru>
2021-01-16 00:51:07 +0300

    Just thought about IReadOnlyList is just garbage, so removing support for it.

--- Algorithm/Sorted/BinarySearchExtensions.cs
+++ Algorithm/Sorted/BinarySearchExtensions.cs
@@ -5,17 +5,6 @@ namespace Algorithm.Sorted
 {
     public static class BinarySearchExtensions
     {
-        private static T GetAt<T>(ICollection<T> collection, int index)
-        {
-            var array1 = collection as IList<T>;
-            if (array1 != null)
-                return array1[index];
-            var array2 = collection as IReadOnlyList<T>;
-            if (array2 != null)
-                return array2[index];
-
-            throw new NotSupportedException("Type of collection is not supported: "+ collection.GetType().ToString());
-        }
         /// <summary>
         /// Use binary search approach to find index in sorted list.
         /// Asymptotic worst case: O(log(n))
@@ -25,7 +14,7 @@ namespace Algorithm.Sorted
         /// <param name="value">Value to search.</param>
         /// <param name="comparer">Comparer to compare values.</param>
         /// <returns>Returns index of element if found and -1 otherwise.</returns>
-        public static int BinarySearchIndexOf<T>(this ICollection<T> collection, T value, IComparer<T> comparer = null)
+        public static int BinarySearchIndexOf<T>(this IList<T> collection, T value, IComparer<T> comparer = null)
         {
             if (collection == null)
                 throw new ArgumentNullException(nameof(collection));
@@ -40,7 +29,7 @@ namespace Algorithm.Sorted
             while (lower <= upper)
             {
                 var middle = lower + ((upper - lower) >> 1);
-                var comparisonResult = comparer.Compare(value, GetAt(collection, middle));
+                var comparisonResult = comparer.Compare(value, collection[middle]);
                 if (comparisonResult == 0)
                     return middle;
                 else if (comparisonResult < 0)
@@ -61,7 +50,7 @@ namespace Algorithm.Sorted
         /// <param name="value">Value to search.</param>
         /// <param name="comparer">Comparer to compare values.</param>
         /// <returns>Index of last element which is not equal and lower than value. Returns (-1) if value is lower than first value and (Count-1) if it is greater than last value.</returns>
-        public static int LowerBoundIndexOf<T>(this ICollection<T> collection, T value, IComparer<T> comparer = null)
+        public static int LowerBoundIndexOf<T>(this IList<T> collection, T value, IComparer<T> comparer = null)
         {
             if (collection == null)
                 throw new ArgumentNullException(nameof(collection));
@@ -75,7 +64,7 @@ namespace Algorithm.Sorted
             while (lower < upper)
             {
                 var middle = lower + ((upper - lower) >> 1);
-                var comparisonResult = comparer.Compare(value, GetAt(collection, middle));
+                var comparisonResult = comparer.Compare(value, collection[middle]);
                 if (comparisonResult <= 0)
                 {
                     upper = middle;
@@ -97,7 +86,7 @@ namespace Algorithm.Sorted
         /// <param name="value">Value to search.</param>
         /// <param name="comparer">Comparer to compare values.</param>
         /// <returns>Index of first element which is not equal and greater than value. Returns (Count) if value is greater than last value and (0) if it is lower than first value.</returns>
-        public static int UpperBoundIndexOf<T>(this ICollection<T> collection, T value, IComparer<T> comparer = null)
+        public static int UpperBoundIndexOf<T>(this IList<T> collection, T value, IComparer<T> comparer = null)
         {
             if (collection == null)
                 throw new ArgumentNullException(nameof(collection));
@@ -111,7 +100,7 @@ namespace Algorithm.Sorted
             while (lower < upper)
             {
                 var middle = lower + ((upper - lower) >> 1);
-                var comparisonResult = comparer.Compare(value, GetAt(collection, middle));
+                var comparisonResult = comparer.Compare(value, collection[middle]);
                 if (comparisonResult >= 0)
                 {
                     lower = middle + 1;
