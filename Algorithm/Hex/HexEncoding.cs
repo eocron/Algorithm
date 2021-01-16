89564d0b7b739a09fe4a380627d134e0de057edb
eocron <ad-company@mail.ru>
2021-01-17 01:11:23 +0300

    Added Hex encoding. This usefull class can have variety of usages.

---
+++ Algorithm/Hex/HexEncoding.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm.Hex
+{
+    [Flags]
+    public enum HexFormatting : uint
+    {
+        /// <summary>
+        /// No prefix in hex string or Unix prefix.
+        /// </summary>
+        Default = None | Unix,
+        /// <summary>
+        /// No prefix in hex string.
+        /// </summary>
+        None = 1 << 0,
+        /// <summary>
+        /// Additinal prefix in hex format: 0x
+        /// </summary>
+        Unix = 1 << 1,
+        /// <summary>
+        /// Additinal prefix in hex format: \x
+        /// </summary>
+        Esc = 1 << 2,
+        /// <summary>
+        /// Additinal prefix in hex format: %
+        /// </summary>
+        Uri = 1 << 3,
+        /// <summary>
+        /// Additinal prefix in hex format: &#x
+        /// </summary>
+        Xml = 1 << 4,
+        /// <summary>
+        /// Additinal prefix in hex format: U+
+        /// </summary>
+        Unicode = 1 << 5,
+        /// <summary>
+        /// Additinal prefix in hex format: #
+        /// </summary>
+        HtmlColor = 1 << 6
+    }
+
+    public static class HexEncoding
+    {
+        private static readonly string[] PossiblePrefixes = new string[]
+        {
+            "0x",//unix
+            "\\x",//esc
+            "%",//uri
+            "&#x",//xml
+            "U+",//unicode
+            "#",//html color
+        };
+
+        private static IEnumerable<string> GetFilteredPrefixes(HexFormatting formatting)
+        {
+            var bitMap = (uint)formatting;
+            return PossiblePrefixes.Where((x, i) => (bitMap & (1 << (i + 1))) != 0);
+        }
+
+        /// <summary>
+        /// Convert byte sequence to HEX representation.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="formatting">HEX string formatting.</param>
+        /// <param name="upperCase">Should hex litteral be in upper case.</param>
+        /// <returns>Formatted HEX representation of bytes.</returns>
+        public static string Convert(Span<byte> bytes, HexFormatting formatting = HexFormatting.Default, bool upperCase = false)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            var prefix = GetFilteredPrefixes(formatting).FirstOrDefault();
+            var offset = prefix?.Length ?? 0;
+
+            var c = new char[offset + bytes.Length * 2];
+            for (int i = 0; i < offset; i++)
+                c[i] = prefix[i];
+
+            byte b;
+            var letterOffset = upperCase ? 0x17 + 0x20 : 0x37 + 0x20;
+            for (int bx = 0, cx = 0; bx < bytes.Length; ++bx, ++cx)
+            {
+                b = ((byte)(bytes[bx] >> 4));
+                c[offset + cx] = (char)(b > 9 ? b + letterOffset : b + 0x30);
+
+                b = ((byte)(bytes[bx] & 0x0F));
+                c[offset + (++cx)] = (char)(b > 9 ? b + letterOffset : b + 0x30);
+            }
+
+            return new string(c);
+        }
+
+        /// <summary>
+        /// Convert HEX representation to bytes.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static byte[] Convert(string str, HexFormatting formatting = HexFormatting.Default, int offset = -1, int count = -1)
+        {
+            if (str == null)
+                throw new ArgumentNullException(nameof(str));
+
+            offset = offset < 0 ? 0 : offset;
+            count = count < 0 ? str.Length : count;
+            
+            var prefix = GetFilteredPrefixes(formatting).FirstOrDefault(x => str.StartsWith(x));
+            if (prefix != null)
+            {
+                offset += prefix.Length;
+                count -= prefix.Length;
+            }
+            else if(!formatting.HasFlag(HexFormatting.None))
+                throw new ArgumentOutOfRangeException(nameof(str), "Invalid hex format.");
+
+            if (count % 2 != 0)
+                throw new ArgumentOutOfRangeException(nameof(str), "Invalid hex length.");
+
+            if (count == 0)
+                return new byte[0];
+            
+            var buffer = new byte[count / 2];
+            char c;
+            for (int bx = 0, sx = 0; bx < buffer.Length; ++bx, ++sx)
+            {
+                c = str[sx+offset];
+                buffer[bx] = (byte)((c > '9' ? (c > 'Z' ? (c - 'a' + 10) : (c - 'A' + 10)) : (c - '0')) << 4);
+
+                c = str[++sx + offset];
+                buffer[bx] |= (byte)(c > '9' ? (c > 'Z' ? (c - 'a' + 10) : (c - 'A' + 10)) : (c - '0'));
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ NTests/HexTests.cs
@@ -0,0 +1,44 @@
+﻿using Algorithm.Hex;
+using NUnit.Framework;
+using System;
+
+namespace NTests
+{
+    [TestFixture]
+    public class HexTests
+    {
+        [Test]
+        [TestCase("0x01aB", HexFormatting.Unix, "0x01AB")]
+        [TestCase("01aB", HexFormatting.None, "01AB")]
+        [TestCase("\\x01aB", HexFormatting.Esc, "\\x01AB")]
+        [TestCase("%01aB", HexFormatting.Uri, "%01AB")]
+        [TestCase("&#x01aB", HexFormatting.Xml, "&#x01AB")]
+        [TestCase("U+01aB", HexFormatting.Unicode, "U+01AB")]
+        [TestCase("#01aB", HexFormatting.HtmlColor, "#01AB")]
+        [TestCase("0x", HexFormatting.Unix, "0x")]
+        [TestCase("", HexFormatting.None, "")]
+        [TestCase("\\x", HexFormatting.Esc, "\\x")]
+        [TestCase("%", HexFormatting.Uri, "%")]
+        [TestCase("&#x", HexFormatting.Xml, "&#x")]
+        [TestCase("U+", HexFormatting.Unicode, "U+")]
+        [TestCase("#", HexFormatting.HtmlColor, "#")]
+        public void Convert(string input, HexFormatting formatting, string expected)
+        {
+            var bytes = HexEncoding.Convert(input, formatting);
+            var actual = HexEncoding.Convert(bytes, formatting, upperCase: true);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("", HexFormatting.Unix, "Invalid hex format. (Parameter 'str')")]
+        [TestCase("0x0", HexFormatting.Unix, "Invalid hex length. (Parameter 'str')")]
+        [TestCase(null, HexFormatting.Unix, "Value cannot be null. (Parameter 'str')")]
+        public void ConvertError(string input, HexFormatting formatting, string expectedError)
+        {
+            Assert.That(() => HexEncoding.Convert(input, formatting),
+               Throws.Exception
+                 .InstanceOf<ArgumentException>()
+                 .With.Message.EqualTo(expectedError));
+        }
+    }
+}
