f11fc6d220e09919da80e4f12abe8d5c0544baf1
eocron <ad-company@mail.ru>
2022-04-09 11:18:10 +0300

    description

--- Algorithm/Queues/IPriorityQueue.cs
+++ Algorithm/Queues/IPriorityQueue.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 
 namespace Eocron.Algorithms.Queues
 {
+    /// <summary>
+    /// Priority queue interface. Represents queue in which order of element affect Equeue/Dequeue operations
+    /// in such way, that at any given time Dequeue will return first priority items.
+    /// </summary>
+    /// <typeparam name="TPriority"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
     public interface IPriorityQueue<TPriority, TValue>
     {
+        /// <summary>
+        /// Enqueue new item.
+        /// </summary>
+        /// <param name="item"></param>
         void Enqueue(KeyValuePair<TPriority, TValue> item);
 
+        /// <summary>
+        /// Inserts new item in queue, or update item if it is already exist.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="onUpdate"></param>
         void EnqueueOrUpdate(KeyValuePair<TPriority, TValue> item, Func<KeyValuePair<TPriority, TValue>, KeyValuePair<TPriority, TValue>> onUpdate);
 
+        /// <summary>
+        /// Dequeue first priority item.
+        /// Throws error if queue is empty.
+        /// </summary>
+        /// <returns></returns>
         KeyValuePair<TPriority, TValue> Dequeue();
 
+        /// <summary>
+        /// Peek first priority item.
+        /// Throws error if queue is empty.
+        /// </summary>
+        /// <returns></returns>
         KeyValuePair<TPriority, TValue> Peek();
 
         void Clear();
