61d066b0c1d1304709a04a6d409b52f89c088874
eocron <ad-company@mail.ru>
2022-04-11 01:35:54 +0300

    Added test description.

--- NTests/DijkstraTests.cs
+++ NTests/DijkstraTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Text.RegularExpressions;
 using Eocron.Algorithms.Graphs;
 using NUnit.Framework;
 using QuikGraph;
@@ -14,36 +13,36 @@ namespace NTests
         [TestCase(new[] {2, 1, 1, 1}, 2)]
         [TestCase(new[] {1, 2, 3, 4}, 2)]
         [TestCase(new[] {4, 3, 2, 1}, 1)]
-        public void Test(int[] pathToRome, int minLength)
+        public void PathToRome(int[] pathToRome, int expectedMinSteps)
         {
             var graph = ParsePathToRome(pathToRome);
-            var result = DijkstraAlgorithm<int, int>.Search(0,
+            var source = 0;
+            var target = pathToRome.Length - 1;
+            var result = DijkstraAlgorithm<int, int>.Search(
+                source,
                 x => graph.OutEdges(x).Select(y => y.Target),
                 x => 0,
                 (x, y) => x.Weight + 1);
-            Assert.AreEqual(minLength, result.Weights[pathToRome.Length - 1]);
+            Assert.AreEqual(expectedMinSteps, result.Weights[target]);
         }
 
-
-        //private static AdjacencyGraph<int, Edge<int>> Parse(string input)
-        //{
-        //    var result = new AdjacencyGraph<int, Edge<int>>();
-        //    foreach (var m in Regex.Matches(input, @"(?<edge>\d+\s*\-\>\s*\d+)|(?<vertex>\d+)").Cast<Match>())
-        //    {
-        //        if (m.Groups["vertex"].Success)
-        //        {
-        //            result.AddVertex(int.Parse(m.Groups["vertex"].Value));
-        //        }
-        //        else if (m.Groups["edge"].Success)
-        //        {
-        //            var split = m.Groups["edge"].Value.Split("->");
-        //            result.AddEdge(new Edge<int>(int.Parse(split[0]), int.Parse(split[1])));
-        //        }
-        //    }
-
-        //    return result;
-        //}
-
+        /// <summary>
+        /// Path to rome is a game. Each index represents city, each value represents range of adjacent cities (i.e from i+1 to i+array[i]).
+        /// Last index is Rome. You need to travel from first index to last in least amount of steps.
+        /// Example:
+        /// 1  2  1  3  1  1  1
+        ///  \/
+        /// 
+        /// 1  2  1  3  1  1  1
+        ///    \_/__/
+        /// 
+        /// 1  2  1  3  1  1  1
+        ///          \_/__/__/
+        ///
+        /// Which is 3 steps.
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
         private static AdjacencyGraph<int, Edge<int>> ParsePathToRome(int[] paths)
         {
             var result = new AdjacencyGraph<int, Edge<int>>();
