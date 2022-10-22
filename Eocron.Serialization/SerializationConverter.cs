0437477e0092a9ec338a958321cf4f2d101fefb9
eocron <ad-company@mail.ru>
2022-10-22 11:40:25 +0300

    fixed static initialization

--- Eocron.Serialization/SerializationConverter.cs
+++ Eocron.Serialization/SerializationConverter.cs
@@ -11,28 +11,43 @@ namespace Eocron.Serialization
 {
     public static class SerializationConverter
     {
-        public static readonly ISerializationConverter Json = new JsonSerializationConverter();
-        public static readonly ISerializationConverter Protobuf = new ProtobufSerializationConverter();
-        public static readonly ISerializationConverter Yaml = new YamlSerializationConverter();
+        static SerializationConverter()
+        {
+            DefaultIndent = true;
+            DefaultBufferSize = 1024;
+            DefaultEncoding = new UTF8Encoding(false);
 
-        public static readonly ISerializationConverter XmlDocument =
-            new XmlSerializationConverter<XmlDocument>(
-                new XmlAdapter<XmlDocument>(
-                    new XmlSerializerAdapter(x => new XmlSerializer(x)),
-                    new XmlDocumentAdapter()));
-        public static readonly ISerializationConverter XDocument =
-            new XmlSerializationConverter<XDocument>(
-                new XmlAdapter<XDocument>(
-                    new XmlSerializerAdapter(x => new XmlSerializer(x)),
-                    new XDocumentAdapter()));
-        public static readonly ISerializationConverter XmlDataContract =
-            new XmlSerializationConverter<XmlDocument>(
-                new XmlAdapter<XmlDocument>(
-                    new XmlObjectSerializerAdapter(x => new DataContractSerializer(x)),
-                    new XmlDocumentAdapter()));
+            Json = new JsonSerializationConverter();
+            Protobuf = new ProtobufSerializationConverter();
+            Yaml = new YamlSerializationConverter();
 
-        public static bool DefaultIndent = true;
-        public static Encoding DefaultEncoding = new UTF8Encoding(false);
-        public static int DefaultBufferSize = 1024;
+            XmlDocument =
+                new XmlSerializationConverter<XmlDocument>(
+                    new XmlAdapter<XmlDocument>(
+                        new XmlSerializerAdapter(x => new XmlSerializer(x)),
+                        new XmlDocumentAdapter()));
+            XDocument =
+                new XmlSerializationConverter<XDocument>(
+                    new XmlAdapter<XDocument>(
+                        new XmlSerializerAdapter(x => new XmlSerializer(x)),
+                        new XDocumentAdapter()));
+            XmlDataContract =
+                new XmlSerializationConverter<XmlDocument>(
+                    new XmlAdapter<XmlDocument>(
+                        new XmlObjectSerializerAdapter(x => new DataContractSerializer(x)),
+                        new XmlDocumentAdapter()));
+        }
+
+        public static readonly ISerializationConverter Json;
+        public static readonly ISerializationConverter Protobuf;
+        public static readonly ISerializationConverter Yaml;
+
+        public static readonly ISerializationConverter XmlDocument;
+        public static readonly ISerializationConverter XDocument;
+        public static readonly ISerializationConverter XmlDataContract;
+
+        public static bool DefaultIndent;
+        public static Encoding DefaultEncoding;
+        public static int DefaultBufferSize;
     }
 }
