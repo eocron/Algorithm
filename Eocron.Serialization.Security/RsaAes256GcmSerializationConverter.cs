99ffb553dc7a0e76015163953748b47c9fac93aa
eocron <ad-company@mail.ru>
2023-11-08 15:17:14 +0300

    More tests

--- Eocron.Serialization.Security/RsaAes256GcmSerializationConverter.cs
+++ Eocron.Serialization.Security/RsaAes256GcmSerializationConverter.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Buffers;
 using System.IO;
+using System.Security;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Eocron.Serialization.Security
 {
-    public sealed class RsaSerializationConverter : ISerializationConverter
+    public sealed class RsaAes256GcmSerializationConverter : ISerializationConverter
     {
         private readonly ISerializationConverter _inner;
         private readonly X509Certificate2 _cert;
         private readonly RSAEncryptionPadding _padding;
         private readonly ArrayPool<byte> _pool;
 
-        public RsaSerializationConverter(ISerializationConverter inner, X509Certificate2 cert, ArrayPool<byte> pool = null)
+        public RsaAes256GcmSerializationConverter(ISerializationConverter inner, X509Certificate2 cert, ArrayPool<byte> pool = null)
         {
             _inner = inner ?? throw new ArgumentNullException(nameof(inner));
             _cert = cert ?? throw new ArgumentNullException(nameof(cert));
@@ -23,6 +24,10 @@ namespace Eocron.Serialization.Security
 
         public object DeserializeFrom(Type type, StreamReader sourceStream)
         {
+            if (!_cert.HasPrivateKey)
+            {
+                throw new SecurityException("RSA certificate should have private key initialized to perform deserialization.");
+            }
             using var rsa = _cert.GetRSAPrivateKey();
             var br = new BinaryReader(sourceStream.BaseStream);
             var encryptedKey = br.ReadBytes(rsa.KeySize / 8);
--- Eocron.Serialization.Tests/RsaSerializationTests.cs
+++ Eocron.Serialization.Tests/RsaSerializationTests.cs
@@ -1,6 +1,10 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Eocron.Serialization.Security;
+using Eocron.Serialization.Tests.Models.Json;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Eocron.Serialization.Tests
@@ -18,16 +22,37 @@ namespace Eocron.Serialization.Tests
             _publicCertificate = TestCertificateHelper.CreatePublicCertificate(_privateCertificate);
         }
 
-        [OneTimeTearDown]
-        public void Teardown()
-        {
-            
-        }
         public override ISerializationConverter GetConverter()
         {
-            var tmp = new RSACryptoServiceProvider();
-            tmp.ExportParameters(false);
-            return  new RsaSerializationConverter(SerializationConverter.Json, _privateCertificate);
+            return GetConverter(_privateCertificate);
+        }
+
+        private static ISerializationConverter GetConverter(X509Certificate2 certificate)
+        {
+            return  new RsaAes256GcmSerializationConverter(SerializationConverter.Json, certificate);
+        }
+
+        [Test]
+        public void EncryptWithPublicThenDecryptWithPrivate()
+        {
+            var publicConverter = GetConverter(_publicCertificate);
+            var privateConverter = GetConverter(_privateCertificate);
+            
+            var model = new JsonTestModel() { Guid = Guid.NewGuid(), FooBarString = "some_string"};
+            var data = publicConverter.SerializeToBytes(model);
+            var decryptedModel = privateConverter.Deserialize<JsonTestModel>(data);
+            decryptedModel.FooBarString.Should().Be("some_string");
+            decryptedModel.Should().BeEquivalentTo(model);
+        }
+        
+        [Test]
+        public void EncryptWithPublicThenDecryptWithPublicShouldFail()
+        {
+            var publicConverter = GetConverter(_publicCertificate);
+            
+            var model = new JsonTestModel() { Guid = Guid.NewGuid(), FooBarString = "some_string"};
+            var data = publicConverter.SerializeToBytes(model);
+            Assert.Throws<SecurityException>(()=> publicConverter.Deserialize<JsonTestModel>(data));
         }
     }
 }
