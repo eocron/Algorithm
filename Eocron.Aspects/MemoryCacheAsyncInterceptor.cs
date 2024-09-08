420ed1d233b7eac171fc2b43e701a70f5961f6fa
eocron <ad-company@mail.ru>
2024-09-09 00:35:42 +0300

    cleanup

--- Eocron.Aspects/MemoryCacheAsyncInterceptor.cs
+++ Eocron.Aspects/MemoryCacheAsyncInterceptor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
 using Eocron.Aspects.Caching;
