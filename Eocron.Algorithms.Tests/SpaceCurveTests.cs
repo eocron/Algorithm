e880d00d25db9d4583c8e812250b6c1c1ae45b27
eocron <ad-company@mail.ru>
2025-07-29 23:22:34 +0300

    z-curve example added

---
+++ Eocron.Algorithms.Tests/SpaceCurveTests.cs
@@ -0,0 +1,18 @@
+﻿using Eocron.Algorithms.SpaceCurves;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Eocron.Algorithms.Tests
+{
+    [TestFixture]
+    public class SpaceCurveTests
+    {
+        [Test]
+        public void ZCurve_Check()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var actual = ZCurve.Interleave(input, 4);
+            actual.Should().Equal(new[] {1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12});
+        }
+    }
+}
---
+++ Eocron.Algorithms/SpaceCurves/ZCurve.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Eocron.Algorithms.SpaceCurves
+{
+    public static class ZCurve
+    {
+        public static T[] Interleave<T>(IReadOnlyList<T> items, int n)
+        {
+            if (items.Count % n != 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), "Array must be a multiple of n");
+            }
+            var output = new T[items.Count];
+            var m = output.Length / n;
+            for (var i = 0; i < output.Length; i++)
+            {
+                output[GetInterleavedIndex(i, n, m)] = items[i];
+            }
+            return output;
+        }
+        
+        public static int GetInterleavedIndex(int idx, int n, int m)
+        {
+            var i = idx / n;
+            var j = idx % n;
+            return j * m + i;
+        }
+    }
+}
