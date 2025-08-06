17cb8dc84de6c092d54448f810c7f8b23e58f0b8
eocron <ad-company@mail.ru>
2025-08-06 23:14:08 +0300

    fix curve

--- Eocron.Algorithms.Tests/SpaceCurveTests.cs
+++ Eocron.Algorithms.Tests/SpaceCurveTests.cs
@@ -8,11 +8,28 @@ namespace Eocron.Algorithms.Tests
     public class SpaceCurveTests
     {
         [Test]
-        public void ZCurve_Check()
+        public void ZCurve_Check_Base3()
         {
             var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-            var actual = ZCurve.Interleave(input, 4);
+            var actual = ZCurve.Interleave(input, 3);
             actual.Should().Equal(new[] {1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12});
+        }
+        
+        [Test]
+        public void ZCurve_Check_Base2()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var actual = ZCurve.Interleave(input, 2);
+            actual.Should().Equal(new[] {1, 7, 2, 8, 3, 9, 4, 10, 5, 11, 6, 12});
+        }
+        
+        [Test]
+        public void ZCurve_Check_Multiple()
+        {
+            var input1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var input2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var actual = ZCurve.Interleave(input1, input2);
+            actual.Should().Equal(new[] {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12});
         }
     }
 }
--- Eocron.Algorithms/SpaceCurves/ZCurve.cs
+++ Eocron.Algorithms/SpaceCurves/ZCurve.cs
@@ -1,23 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Eocron.Algorithms.SpaceCurves
 {
     public static class ZCurve
     {
-        public static T[] Interleave<T>(IReadOnlyList<T> items, int n)
+        public static T[] Interleave<T>(IReadOnlyList<T> items, int m)
         {
-            if (items.Count % n != 0)
+            return Interleave<T>(items, items.Count, m);
+        }
+        
+        
+        public static T[] Interleave<T>(IEnumerable<T> items, int size, int m)
+        {
+            if (size % m != 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(n), "Array must be a multiple of n");
+                throw new ArgumentOutOfRangeException(nameof(m), "Array must be a multiple of " + m);
             }
-            var output = new T[items.Count];
-            var m = output.Length / n;
-            for (var i = 0; i < output.Length; i++)
+            var output = new T[size];
+            var n = size / m;
+            var i = 0;
+            foreach (var item in items)
             {
-                output[GetInterleavedIndex(i, n, m)] = items[i];
+                output[GetInterleavedIndex(i, n, m)] = item;
+                i++;
             }
             return output;
+        }
+        
+        public static T[] Interleave<T>(params IReadOnlyList<T>[] itemSequences)
+        {
+            if (itemSequences.Select(x=> x.Count).Distinct().Count()>1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(itemSequences), "Sequences should have same size");
+            }
+
+            var m = itemSequences.Length;
+            return Interleave(itemSequences.SelectMany(x => x), itemSequences.Sum(x => x.Count), m);
         }
         
         public static int GetInterleavedIndex(int idx, int n, int m)
